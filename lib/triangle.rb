65afe15d44459143da8d669b0395c9bad20ca52a
angpace <108838753+angpace@users.noreply.github.com>
2022-11-02 14:10:47 -0400

    commit tree 70ce8313b7c56ac387f2795e92dd509c3a66a070

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
   # write code here
+  attr_reader :a, :b, :c 
+  attr_writer :a, :b, :c
+
+  def initialize (a, b, c)
+    @a = a
+    @b = b 
+    @c = c 
+  end 
+
+ def kind 
+  if (@a <= 0 || @b <= 0 || c <= 0)
+    raise TriangleError
+  elsif (@a + @b <= @c || @b + @c <= @a || @a + @c <= @b)
+    raise TriangleError 
+  elsif (@a == @b && @b == @c)
+    :equilateral
+  elsif (@a == @b || @b == @c || @c == @a)
+    :isosceles 
+  else
+    :scalene
+  end
+
+ end
+
+
+
+ class TriangleError < StandardError
+
+end
+
 end
